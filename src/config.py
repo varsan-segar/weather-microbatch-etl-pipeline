92b71b436c5d89ef5fb44b3e46a8fa99c6231e7b
S S VARSAN <varsansegars@gmail.com>
2026-02-20 01:20:44 +0530

    Add config module for credential management

---
+++ src/config.py
@@ -0,0 +1,14 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv("API_KEY")
+
+DB_CONFIG = {
+    'host':os.getenv("HOST_NAME"),
+    'dbname':os.getenv("DBNAME"),
+    'port':os.getenv("PORT"),
+    'user':os.getenv("USER"),
+    'password':os.getenv("PASSWORD")
+}
